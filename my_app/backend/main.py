6a71bdda24c5f62a264397d2f829b7f86851cb01
Kyriakos Fragkeskos <kyriakos.frangeskos@gmail.com>
2020-07-24 12:21:00 +0300

    Simple RestAPI with mongodb

---
+++ my_app/backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from config import config
+from routes.core import core_router
+
+api_path = f"/api/{config.API_VERSION}"
+
+app = FastAPI(title="MyApp", version="0.1")
+
+app.include_router(
+    core_router, tags=["core"], responses={404: {"description": "Not Found"}}
+)
+
+
+@app.on_event("startup")
+async def app_startup():
+    config.load_config()
+
+
+@app.on_event("shutdown")
+async def app_shutdown():
+    config.close_db_client()
